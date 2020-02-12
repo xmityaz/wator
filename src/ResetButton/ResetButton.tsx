8aa9c4b1d05c89389e4eba367e929e0c6acb8983
xmityaz <dmytron@wix.com>
2020-02-12 21:18:50 +0200

    button styling imrpovements

--- src/ResetButton/ResetButton.tsx
+++ src/ResetButton/ResetButton.tsx
@@ -9,6 +9,10 @@ export class ResetButton extends React.Component<ResetButtonProps> {
   render() {
     return (
       <button className={s.root} onClick={this.props.onClick}>
+        <span className={s.icon}>
+          <span className={s.iconBorder} />
+          <span className={s.iconArrow} />
+        </span>
         Reset
       </button>
     );

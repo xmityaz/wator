e8431605744e44d7526c4ae9a8026a48f9f1e436
xmityaz <dmytron@wix.com>
2020-02-22 15:41:48 +0200

    mobile optimization

---
+++ precache-manifest.6f09a03422885362669999d1a881292b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc56a49b3d9270993c307b4920abea85",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38366092a471c612553b",
+    "url": "/static/css/2.96479aa2.chunk.css"
+  },
+  {
+    "revision": "2ba2583ae4c4837db957",
+    "url": "/static/css/main.612e6c47.chunk.css"
+  },
+  {
+    "revision": "38366092a471c612553b",
+    "url": "/static/js/2.1d30b8af.chunk.js"
+  },
+  {
+    "revision": "2ba2583ae4c4837db957",
+    "url": "/static/js/main.88e84ab2.chunk.js"
+  },
+  {
+    "revision": "e0572293603fcb046f4b",
+    "url": "/static/js/runtime-main.e64b5528.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a3b98684e270d3999179045e7edb5c22.js"
+  "/precache-manifest.6f09a03422885362669999d1a881292b.js"
 );
 
 self.addEventListener('message', (event) => {
